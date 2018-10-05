8ab909e42a609b2a1e8b50b64cbd6c6261d2b90b
rahmanashik <36976260+rahmanashik@users.noreply.github.com>
2018-10-05 20:45:32 +0930

    Delete app.js
    updated

--- js/app.js
+++
@@ -1,8 +0,0 @@
-if('serviceWorker' in navigator){
-    try {
-        navigator.serviceWorker.register('sw.js');
-        console.log('service worker registered')
-    } catch (error) {
-        console.log(error);
-    }
-}
